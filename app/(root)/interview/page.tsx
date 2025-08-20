3ab58d0c6143c57bfa4aea2b80b2f0f9e3b8dad0
MradulGandhi <gandhisir80@gmail.com>
2025-08-20 15:56:52 +0530

    env removal

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,14 @@
+import Agent from '@/components/agent'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <>
+        <h3>Interview Generation</h3>
+        
+        <Agent U/>
+    </>
+  )
+}
+
+export default Page
